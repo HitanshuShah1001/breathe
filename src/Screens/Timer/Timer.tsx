cdd3bda1f406dff12e7c40367ce3b2d14d493c2a
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-10 03:30:03 +0530

    feat:Pause completed

--- src/Screens/Timer/Timer.tsx
+++ src/Screens/Timer/Timer.tsx
@@ -1,36 +1,46 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, Dimensions,Button } from 'react-native'
 
 interface Props {
   duration: Number
 }
 export default function Timer({ duration }: Props) {
-  const {width, height} = Dimensions.get('window');
-  const [minutes, setMinutes] = useState<Number>(0)
-  const [seconds, setSeconds] = useState<Number>(0)
+
+  const [minutes, setMinutes] = useState<number>(0)
+  const [seconds, setSeconds] = useState<number>(0)
+  const [time,setTime] = useState<number>(600);
+  const [pause,setPause] = useState<number>(false);
+
   const durationRef = useRef(null)
-  const totalDuration = Date.now() + 1000 * (30 + 2)
+
 
   const getTime = () => {
-    const time = totalDuration - Date.now()
+    setTime(time-1);
+    console.log(time);
     if (time <= 0) {
       clearInterval(durationRef.current)
       durationRef.current = null
     }
-    setMinutes(Math.floor((time / 1000 / 60) % 60))
-    setSeconds(Math.floor((time / 1000) % 60))
+    setSeconds(Math.floor(time%60))
+    setMinutes(Math.floor(time/60))
   }
   useEffect(() => {
-    durationRef.current = setInterval(() => {
-      getTime()
-    }, 1000)
+    if(!pause){
+      durationRef.current = setInterval(() => {
+        setTime(time-1);
+        setSeconds(Math.floor(time%60))
+      setMinutes(Math.floor(time/60))
+      }, 1000)
+    }
+   
 
     return () => clearInterval(durationRef.current)
-  }, [])
+  }, [time,pause])
   return (
     <View style={{flex:1,flexDirection:'row',justifyContent:'center',marginTop:25}}>
       <Text>{minutes < 0 ? 0 : minutes} : </Text>
       <Text>{seconds < 0 ? 0 : seconds}</Text>
+      <Button title={pause?'Resume':'Pause'} onPress={() => setPause(!pause)}/>
     </View>
   )
 }
