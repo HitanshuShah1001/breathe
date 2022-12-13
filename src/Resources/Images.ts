b4aa643d567ce0c86a4daeedb2a752d60bb632c0
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-14 00:46:51 +0530

    feat:Image paths added

---
+++ src/Resources/Images.ts
@@ -0,0 +1,6 @@
+export const Angry: String = require("../../assets/Photos/Angry/Angry.jpeg");
+export const Cluttered: String = require("../../assets/Photos/Cluttered/Cluttered.jpeg");
+export const Excited: String = require("../../assets/Photos/Excited/Excited.gif");
+export const Insomnia: String = require("../../assets/Photos/Insomnia/Insomnia.jpeg");
+export const Stressed: String = require("../../assets/Photos/Stressed/Stressed.jpeg");
+export const Tired: String = require("../../assets/Photos/Tired/Tired.jpeg");
