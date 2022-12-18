98a5834c15c81354737ee4d17c73ae425a6e4844
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-18 21:08:51 +0530

    feat:Breathe iconadded

---
+++ src/Components/Breatheicon/Breatheicon.tsx
@@ -0,0 +1,8 @@
+import {Image} from 'react-native';
+
+
+export default function Breatheicon() {
+    return (
+        <Image source={require('../../../assets/Photos/meditate.png')}  style={{width:20,height:20}}/>
+    )
+}
