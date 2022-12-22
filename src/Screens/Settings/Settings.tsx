90d62c9d55f6ab3ed23ebd4611ba1d7dbff601f1
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-22 23:06:35 +0530

    feat:Settings screen dark mode functionaltiy

--- src/Screens/Settings/Settings.tsx
+++ src/Screens/Settings/Settings.tsx
@@ -1,36 +1,40 @@
-import { useContext, useState } from 'react'
-import { View, Text, StyleSheet, Switch, SafeAreaView } from 'react-native'
-import { styles } from './styles'
-import { Context } from '../../Statemanagement/Context'
-import { Light,Dark } from '../../Utils/Constants/Colors'
+import { useContext, useState } from "react";
+import { View, Text, StyleSheet, Switch, SafeAreaView } from "react-native";
+import { styles } from "./styles";
+import { Context } from "../../Statemanagement/Context";
+import { Light, Dark } from "../../Utils/Constants/Colors";
+import { LinearGradient } from "expo-linear-gradient";
 export default function Settings() {
-  const [enabled, setEnabled] = useState(false)
-  const {Lightmode,setLightMode,colors,setColors} = useContext(Context);
+  const [enabled, setEnabled] = useState(false);
+  const { Lightmode, setLightMode, colors, setColors } = useContext(Context);
 
   const toggleMode = () => {
     setLightMode(!Lightmode);
-    setEnabled((enabled) => !enabled)
-    setColors(Lightmode == false ? Light:Dark)
-  }
+    setEnabled((enabled) => !enabled);
+    setColors(Lightmode == false ? Light : Dark);
+  };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={styles.container}
+      <LinearGradient
+        style={{ flex: 1, justifyContent: "space-around" }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        colors={colors.Background}
       >
-        <View
-          style={styles.subcontainer}
-        >
-          <Text style={styles.text}>Dark Mode</Text>
-          <Switch
-            trackColor={{ false: '#767577', true: '#81b0ff' }}
-            thumbColor={enabled ? '#f5dd4b' : '#f4f3f4'}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleMode}
-            value={enabled}
-          />
+        <View style={styles.container}>
+          <View style={styles.subcontainer}>
+            <Text style={[styles.text, { color: colors.Text }]}>Dark Mode</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: "#81b0ff" }}
+              thumbColor={enabled ? "#f5dd4b" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleMode}
+              value={enabled}
+            />
+          </View>
         </View>
-      </View>
+      </LinearGradient>
     </SafeAreaView>
-  )
+  );
 }
