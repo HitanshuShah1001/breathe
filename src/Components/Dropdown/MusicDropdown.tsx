dd3cd4fd45f82cabb65207fa44fa2a535af66492
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-29 12:58:21 +0530

    Change sound on change of audio

--- src/Components/Dropdown/MusicDropdown.tsx
+++ src/Components/Dropdown/MusicDropdown.tsx
@@ -1,11 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, TouchableOpacity, Text, ScrollView } from "react-native";
+import { Audio } from "expo-av";
 import { styles } from "./styles";
 import { Music } from "../../Resources/Music";
 import { Context } from "../../Statemanagement/Context";
+import { useRoute } from "@react-navigation/native";
+
 export default function MusicDropdown() {
-  const { sound, setSound } = React.useContext(Context);
-  const [show, setShow] = useState(false);
+  const route = useRoute();
+  const { sound: music, setSound } = React.useContext(Context);
+  const [show, setShow] = useState<Boolean>(false);
+  const timerRef = useRef<NodeJS.Timeout | any>(null);
+  const [audio, setAudio] = useState<any>();
+
+  const stopAudioifnavigatedtoanotherroute = async () => {
+    if (route.name !== "Startbreathing") {
+      await audio?.unloadAsync();
+    }
+  };
+  // useEffect(() => {
+  //   stopAudioifnavigatedtoanotherroute();
+  // }, [route]);
+
+  const demoSound = async (mood: string) => {
+    setShow(!show);
+    setSound(mood);
+    await audio?.stopAsync();
+    const { sound } = await Audio.Sound.createAsync(
+      Music[Music.findIndex((x) => x.mood == mood)].audio,
+      {
+        isLooping: true,
+      }
+    );
+
+    await sound.playAsync();
+    setAudio(sound);
+    timerRef.current = setTimeout(() => {
+      sound.stopAsync();
+    }, 10000);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.subcontainer}>
@@ -13,7 +47,7 @@ export default function MusicDropdown() {
           style={styles.dropdown}
           onPress={() => setShow(!show)}
         >
-          <Text style={{ color: "white" }}>{sound}</Text>
+          <Text style={{ color: "white" }}>{music}</Text>
         </TouchableOpacity>
 
         {show && (
@@ -22,10 +56,7 @@ export default function MusicDropdown() {
               return (
                 <TouchableOpacity
                   style={styles.duration}
-                  onPress={() => {
-                    setSound(item.mood);
-                    setShow(!show);
-                  }}
+                  onPress={() => demoSound(item.mood)}
                   key={index}
                 >
                   <Text style={{ color: "white" }}>{item.mood}</Text>
