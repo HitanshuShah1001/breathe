d1760e23e1fb8c9e75c27c5ffbbe32aaa7ab8366
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-30 10:45:05 +0530

    feat:Notifications array added

---
+++ src/Resources/Quotes.ts
@@ -0,0 +1,11 @@
+export const quotes: Array<string> = [
+  "Breath is the bridge which connects life to consciousness, which unites your body to your thoughts",
+  "Breathing is the greatest pleasure in life.",
+  "Breathe. Let go. And remind yourself that this very moment is the only one you know you have for sure.",
+  "Breathing is the first act of life, and the last.",
+  "Breathing is the first thing and the last thing we do on this earth. When we are born, we take our first breath, and when we die, we release our final breath. In between, it is the thing that keeps us alive.",
+  "Breathing is the most essential element of life. The ability to breathe is the very essence of being alive.",
+  "Breathing is the source of all life, and the breath of life is the source of all energy.",
+  "Breathing is not just a physical act, it is also a spiritual act. The breath is a bridge between the physical body and the spiritual self.",
+  "Breathing is a reminder that we are alive, that we have the opportunity to make a positive impact on the world, and that every moment is a chance to begin anew.",
+];
