af6b6d66d63fbdb40530e0ca2a3db47e06c8a3ac
Hitanshu Shah <hitanshushah5@gmail.com>
2022-12-30 10:46:44 +0530

    fix:Removed home screen

--- src/Screens/Home/Header/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-  container: { flex: 0.2, justifyContent: "flex-end", alignItems: "center" },
-  text: {
-    fontFamily: "Secular",
-    fontSize: 20,
-  },
-});
