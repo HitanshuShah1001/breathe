e2fcffcb1a0be6fd55bf4a383271ec124b7385f3
Hitanshu Shah <hitanshushah5@gmail.com>
2023-01-03 23:58:14 +0530

    feat:Incomplete session showing added in settings

--- src/Components/TotalSessions/TotalSessions.tsx
+++ src/Components/TotalSessions/TotalSessions.tsx
@@ -3,17 +3,17 @@ import { View, Text } from "react-native";
 import { Context } from "../../Statemanagement/Context";
 import { styles } from "./styles";
 
-export default function TotalSessions() {
-  const { sessions, colors } = useContext(Context);
+interface Props {
+  text: string;
+  value: number;
+}
+export default function TotalSessions({ text, value }: Props) {
+  const { colors } = useContext(Context);
   return (
     <View style={styles.container}>
-      <Text style={[styles.text, { color: colors.Text }]}>
-        Sessions Completed
-      </Text>
+      <Text style={[styles.text, { color: colors.Text }]}>{text}</Text>
       <View>
-        <Text style={[styles.sessions, { color: colors.Text }]}>
-          {sessions}
-        </Text>
+        <Text style={[styles.sessions, { color: colors.Text }]}>{value}</Text>
       </View>
     </View>
   );
